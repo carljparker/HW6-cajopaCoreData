988274b48faeda32d7da65fa4c3b881cfda2389a
Carl Parker <Carl.Parker@hbo.com>
2015-03-06 20:06:21 -0800

    add a subclass of NSManagedObject for Item entity

---
+++ cajopaCoreData/Item.h
@@ -0,0 +1,37 @@
+//
+//  Item.h
+//  cajopaCoreData
+//
+//  Created by Parker, Carl (HBO) on 3/6/15.
+//  Copyright (c) 2015 Parker, Carl (HBO). All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Item : NSManagedObject
+
+@property (nonatomic, retain) NSString * detail;
+@property (nonatomic) double latitude;
+@property (nonatomic) double longitude;
+@property (nonatomic, retain) NSDecimalNumber * price;
+@property (nonatomic, retain) NSString * title;
+@property (nonatomic, retain) NSString * uuid;
+@property (nonatomic, retain) NSSet *images;
+@property (nonatomic, retain) NSSet *tags;
+@end
+
+@interface Item (CoreDataGeneratedAccessors)
+
+- (void)addImagesObject:(NSManagedObject *)value;
+- (void)removeImagesObject:(NSManagedObject *)value;
+- (void)addImages:(NSSet *)values;
+- (void)removeImages:(NSSet *)values;
+
+- (void)addTagsObject:(NSManagedObject *)value;
+- (void)removeTagsObject:(NSManagedObject *)value;
+- (void)addTags:(NSSet *)values;
+- (void)removeTags:(NSSet *)values;
+
+@end
---
+++ cajopaCoreData/Item.m
@@ -0,0 +1,23 @@
+//
+//  Item.m
+//  cajopaCoreData
+//
+//  Created by Parker, Carl (HBO) on 3/6/15.
+//  Copyright (c) 2015 Parker, Carl (HBO). All rights reserved.
+//
+
+#import "Item.h"
+
+
+@implementation Item
+
+@dynamic detail;
+@dynamic latitude;
+@dynamic longitude;
+@dynamic price;
+@dynamic title;
+@dynamic uuid;
+@dynamic images;
+@dynamic tags;
+
+@end
