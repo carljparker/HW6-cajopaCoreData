19f373b94b21ec8ee84f56d92ac3620ccca9fd29
Carl Parker <Carl.Parker@hbo.com>
2015-03-13 07:23:57 -0700

    add class for location. create relationship between Items and Locs

---
+++ cajopaCoreData/Location.h
@@ -0,0 +1,13 @@
+//
+//  Location.h
+//  cajopaCoreData
+//
+//  Created by Parker, Carl (HBO) on 3/13/15.
+//  Copyright (c) 2015 Parker, Carl (HBO). All rights reserved.
+//
+
+#import <CoreData/CoreData.h>
+
+@interface Location : NSManagedObject
+
+@end
---
+++ cajopaCoreData/Location.m
@@ -0,0 +1,13 @@
+//
+//  Location.m
+//  cajopaCoreData
+//
+//  Created by Parker, Carl (HBO) on 3/13/15.
+//  Copyright (c) 2015 Parker, Carl (HBO). All rights reserved.
+//
+
+#import "Location.h"
+
+@implementation Location
+
+@end
