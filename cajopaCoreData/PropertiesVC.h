1bd232c196f21bc5b4994458cc6add2dcd0c0c17
Carl Parker <Carl.Parker@hbo.com>
2015-03-16 19:29:03 -0700

    enable moc; set lat & long in properties sheet

--- cajopaCoreData/PropertiesVC.h
+++ cajopaCoreData/PropertiesVC.h
@@ -6,6 +6,8 @@
 //  Copyright (c) 2015 Parker, Carl (HBO). All rights reserved.
 //
 
+@import CoreData;
+
 #import <Cocoa/Cocoa.h>
 #import "Item.h"
 #import "Tag.h"
--- cajopaCoreData/PropertiesVC.m
+++ cajopaCoreData/PropertiesVC.m
@@ -10,6 +10,8 @@
 
 @interface PropertiesVC ()
 
+@property (nonatomic, readwrite) NSManagedObjectContext * moc;
+
 @end
 
 @implementation PropertiesVC
@@ -22,6 +24,13 @@
     self.tagListTable.dataSource = self;
     
     self.itemTitleText.stringValue = self.displayedItem.title;
+    
+    NSNumberFormatter *formatter = [NSNumberFormatter new];
+    formatter.numberStyle = NSNumberFormatterDecimalStyle;
+    
+    self.latText.stringValue = [formatter stringFromNumber:[NSNumber numberWithDouble:self.displayedItem.location.latitude]];
+    self.longText.stringValue = [formatter stringFromNumber:[NSNumber numberWithDouble:self.displayedItem.location.longitude]];
+    self.moc = self.displayedItem.managedObjectContext;
 
 }
 
@@ -29,7 +38,7 @@
     // fetch all the tags we have so far
     NSError *fetchError = nil;
     NSFetchRequest *fr = [NSFetchRequest fetchRequestWithEntityName:@"Tag"];
-    NSArray *allTags; //= [self.moc executeFetchRequest:fr error:&fetchError];
+    NSArray *allTags = [self.moc executeFetchRequest:fr error:&fetchError];
     
     // do some logging here
     NSLog(@"ViewDidLoad: %@", allTags);
@@ -80,7 +89,7 @@
     // fetch all the tags we have so far
     NSError *fetchError = nil;
     NSFetchRequest *fr = [NSFetchRequest fetchRequestWithEntityName:@"Tag"];
-    NSArray *allTags; // = [self.moc executeFetchRequest:fr error:&fetchError];
+    NSArray *allTags = [self.moc executeFetchRequest:fr error:&fetchError];
 
     
     // For this to work, Table Cell View in Main.storyboard
